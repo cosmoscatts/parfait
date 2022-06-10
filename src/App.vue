d09de7a55780b19370fdb491adc14dac9de21eb1
duende <lyc0228@@@>
2022-06-10 19:52:22 +0800

    chore: init project

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+const a = 1
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <img alt="Vue logo" src="./assets/logo.png">
 </template>
 
 <style>
