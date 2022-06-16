8f6d55fc2285ad2bddc315e716f31abbb24493fe
faitsse <2138889191@qq.com>
2022-06-16 21:26:24 +0800

    chore: redirect

---
+++ src/pages/redirect/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const route = useRoute()
+const router = useRouter()
+const { params, query } = route
+const { path } = params
+router.replace({ path: `/${path}`, query })
+</script>
+
+<template>
+  <div />
+</template>
--- src/routes/constant.ts
+++ src/routes/constant.ts
@@ -7,8 +7,35 @@ export const constantRoutes = [
     component: BaseLayout,
   },
   {
+    path: '/redirect',
+    component: BaseLayout,
+    meta: {
+      hidden: true,
+      auth: true,
+    },
+    children: [
+      {
+        path: ':path(.*)',
+        component: () => import('~/pages/redirect/index.vue'),
+      },
+    ],
+  },
+  {
     path: '/login',
     name: 'login',
     component: () => import('~/pages/login/index.vue'),
+  },
+  {
+    path: '/404',
+    component: () => import('~/pages/error/index.vue'),
+    meta: {
+      hidden: true,
+      auth: true,
+    },
+  },
+  {
+    path: '/:pathMatch(.*)',
+    name: 'matchAll',
+    redirect: '/404',
   },
 ]
