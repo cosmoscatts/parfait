273aa7bdaa576dcdb4e4b511975c117a8c21039b
faitsse <2138889191@qq.com>
2022-06-16 23:11:17 +0800

    feat: avatar

--- src/layouts/widgets/NavAvatar.vue
+++ src/layouts/widgets/NavAvatar.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
+import avatar from '~/assets/avatar.jpg'
+
 function onSelect() {}
 </script>
 
 <template>
   <div cursor-pointer>
     <a-dropdown trigger="hover" position="bottom" @select="onSelect">
-      <a-avatar :size="40" shape="square">
-        sse
+      <a-avatar :size="32" shape="square">
+        <img
+          alt="头像"
+          :src="avatar"
+        >
       </a-avatar>
       <template #content>
         <a-doption>个人资料</a-doption>
