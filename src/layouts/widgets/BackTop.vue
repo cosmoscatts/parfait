a4c1b5637426df7f1c89a024e9b4a627d90dcb34
faitsse <2138889191@qq.com>
2022-06-17 23:09:39 +0800

    chore: imporve backTop

--- src/layouts/widgets/BackTop.vue
+++ src/layouts/widgets/BackTop.vue
@@ -6,20 +6,16 @@ const {
   targetContainer: string
 }>()
 const { fixHeader } = storeToRefs(useSettingsStore())
+// when the fixHeader change, the backTop target not update
+// immediately, so change the key to force update
+let key = $ref(0)
+watch(fixHeader, () => {
+  key++
+})
 </script>
 
 <template>
-  <a-back-top v-if="fixHeader" :target-container="targetContainer">
-    <a-button
-      fixed shadow-md
-      class="bottom-23% right-3%"
-      size="large"
-      shape="circle"
-    >
-      <icon-to-top />
-    </a-button>
-  </a-back-top>
-  <a-back-top v-else :target-container="targetContainer">
+  <a-back-top :key="key" :target-container="targetContainer">
     <a-button
       fixed shadow-md
       class="bottom-23% right-3%"
