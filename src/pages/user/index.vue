e99af2ee07c75650a2a6990aa491bca825aba7de
faitsse <2138889191@qq.com>
2022-06-19 21:41:24 +0800

    chore: update

--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -32,7 +32,9 @@ function fetchTableData(params: Record<string, any>) {
     // you can report use errorHandler or other
   }
   finally {
-    setLoading(false)
+    useTimeoutFn(() => {
+      setLoading(false)
+    }, 1000)
   }
 }
 const refSearchForm = ref()
@@ -69,34 +71,41 @@ function formatRowIndex(idx: number) {
           <a-table-column
             title="序号"
             data-index="number"
+            :cell-style="{ 'text-align': 'center' }"
           >
             <template #cell="{ rowIndex }">
               {{ formatRowIndex(rowIndex) }}
             </template>
-          </a-table-column>>
+          </a-table-column>
           <a-table-column
             title="账号"
             data-index="username"
+            :cell-style="{ 'text-align': 'center' }"
           />
           <a-table-column
             title="名称"
             data-index="name"
+            :cell-style="{ 'text-align': 'center' }"
           />
           <a-table-column
             title="角色"
             data-index="roleId"
+            :cell-style="{ 'text-align': 'center' }"
           />
           <a-table-column
             title="手机号"
             data-index="phone"
+            :cell-style="{ 'text-align': 'center' }"
           />
           <a-table-column
             title="邮箱"
             data-index="email"
+            :cell-style="{ 'text-align': 'center' }"
           />
           <a-table-column
             title="创建时间"
             data-index="createTime"
+            :cell-style="{ 'text-align': 'center' }"
           >
             <template #cell="{ record }">
               {{ formartDate(record.createTime) }}
@@ -105,6 +114,7 @@ function formatRowIndex(idx: number) {
           <a-table-column
             title="操作"
             data-index="operations"
+            :cell-style="{ 'text-align': 'center' }"
           >
             <template #cell>
               <a-button type="text" size="small">
