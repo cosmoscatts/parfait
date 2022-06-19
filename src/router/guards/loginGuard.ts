9c7670c9930fe17f041c5d0b686170cfd6456065
faitsse <2138889191@qq.com>
2022-06-19 08:32:45 +0800

    chore: update

--- src/router/guards/loginGuard.ts
+++ src/router/guards/loginGuard.ts
@@ -1,8 +1,8 @@
 import type { Router } from 'vue-router'
 import NProgress from 'nprogress'
 
-export default function setupUserLoginInfoGuard(router: Router) {
-  router.beforeEach(async (to, _, next) => {
+export default function setupLoginGuard(router: Router) {
+  router.beforeEach(async (to, form, next) => {
     NProgress.start()
     const { user } = useUserStore()
     if (user) {
@@ -13,6 +13,8 @@ export default function setupUserLoginInfoGuard(router: Router) {
         useLogout()
         Message.error('请联系管理员配置用户角色')
         next('/login')
+        if (form.name === 'login')
+          NProgress.done()
       }
     }
     else {
