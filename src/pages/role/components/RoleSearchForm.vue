f962cb76c7ec6fdd84511d9ccf2e5830314fcaef
faitsse <2138889191@qq.com>
2022-06-19 21:52:03 +0800

    chore: role

---
+++ src/pages/role/components/RoleSearchForm.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { IconRefresh, IconSearch } from '@arco-design/web-vue/es/icon'
+
+const emit = defineEmits(['fetchData'])
+function generateFormModel() {
+  return {
+    name: '',
+    createTime: [],
+  }
+}
+const formModel = ref(generateFormModel())
+function search() {
+  emit('fetchData', formModel.value)
+}
+search()
+function reset() {
+  formModel.value = ref(generateFormModel()) as any
+}
+
+defineExpose({
+  formModel,
+})
+</script>
+
+<template>
+  <a-row>
+    <a-col :flex="1">
+      <a-form
+        :model="formModel"
+        :label-col-props="{ span: 6 }"
+        :wrapper-col-props="{ span: 18 }"
+        label-align="left"
+      >
+        <a-row :gutter="16">
+          <a-col :span="12">
+            <a-form-item field="name" label="名称">
+              <a-input
+                v-model="formModel.name"
+                placeholder="请输入名称"
+              />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item field="createTime" label="创建时间">
+              <a-range-picker
+                v-model="formModel.createTime"
+                w-full
+              />
+            </a-form-item>
+          </a-col>
+        </a-row>
+      </a-form>
+    </a-col>
+    <a-divider class="!h-34px" direction="vertical" />
+    <a-col flex="172px">
+      <a-space :size="18">
+        <a-button type="primary" @click="search">
+          <template #icon>
+            <icon-search />
+          </template>
+          搜索
+        </a-button>
+        <a-button @click="reset">
+          <template #icon>
+            <icon-refresh />
+          </template>
+          重置
+        </a-button>
+      </a-space>
+    </a-col>
+  </a-row>
+</template>
--- src/pages/role/index.vue
+++ src/pages/role/index.vue
@@ -1,5 +1,109 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import RoleSearchForm from './components/RoleSearchForm.vue'
+import type { Pagination } from '~/types'
+
+const { loading, setLoading } = useLoading()
+let tabledata = $ref([])
+const basePagination: Pagination = {
+  current: 1,
+  pageSize: 10,
+}
+const pagination = reactive({
+  ...basePagination,
+})
+function fetchRoleData(params: Record<string, any>) {
+  params = { ...basePagination, ...params }
+  setLoading(true)
+  try {
+    const { data } = RoleApi.fetchRoleList()
+    tabledata = data as any
+    pagination.current = params.current
+    pagination.total = data.length
+  }
+  catch (err) {
+    // you can report use errorHandler or other
+  }
+  finally {
+    useTimeoutFn(() => {
+      setLoading(false)
+    }, 1000)
+  }
+}
+const refSearchForm = ref()
+function onPageChange(current: number) {
+  if (!refSearchForm.value)
+    return
+  const params = refSearchForm.value.formModel
+  fetchRoleData({ ...params, ...basePagination, current })
+}
+function formartDate(date?: Date) {
+  return date
+    ? dayJs(date).format('YYYY-MM-DD HH:mm:ss')
+    : ''
+}
+function formatRowIndex(idx: number) {
+  const { current, pageSize } = pagination
+  return (current - 1) * pageSize + idx + 1
+}
+</script>
 
 <template>
-  <div />
+  <div>
+    <a-card title="查询角色">
+      <RoleSearchForm ref="refSearchForm" @fetchData="fetchRoleData" />
+      <a-table
+        row-key="id"
+        :loading="loading"
+        :pagination="pagination"
+        :data="tabledata"
+        :bordered="false"
+        @page-change="onPageChange"
+      >
+        <template #columns>
+          <a-table-column
+            title="序号"
+            data-index="number"
+            :cell-style="{ 'text-align': 'center' }"
+          >
+            <template #cell="{ rowIndex }">
+              {{ formatRowIndex(rowIndex) }}
+            </template>
+          </a-table-column>
+          <a-table-column
+            title="名称"
+            data-index="name"
+            :cell-style="{ 'text-align': 'center' }"
+          />
+          <a-table-column
+            title="角色描述"
+            data-index="description"
+            :cell-style="{ 'text-align': 'center' }"
+          />
+          <a-table-column
+            title="创建时间"
+            data-index="createTime"
+            :cell-style="{ 'text-align': 'center' }"
+          >
+            <template #cell="{ record }">
+              {{ formartDate(record.createTime) }}
+            </template>
+          </a-table-column>
+          <a-table-column
+            title="操作"
+            data-index="operations"
+            :cell-style="{ 'text-align': 'center' }"
+          >
+            <template #cell>
+              <a-button type="text" size="small">
+                编辑
+              </a-button>
+              <a-button type="text" size="small">
+                删除
+              </a-button>
+            </template>
+          </a-table-column>
+        </template>
+      </a-table>
+    </a-card>
+  </div>
 </template>
