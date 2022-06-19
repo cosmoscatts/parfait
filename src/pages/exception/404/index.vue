e3745e9feffa466c86a8c4e11218aa8030f5cba5
faitsse <2138889191@qq.com>
2022-06-19 14:34:19 +0800

    chore: exception page

--- src/pages/exception/404/index.vue
+++ src/pages/exception/404/index.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
 const router = useRouter()
-const back = () => {
-  // warning： Go to the node that has the permission
+function back() {
   router.back()
 }
 </script>
 
 <template>
-  <div w-screen h-screen flex="~ col" justify-center items-center>
-    <a-result status="404" subtitle="页面不存在" />
-    <a-button key="back" type="primary" @click="back">
-      返回
-    </a-button>
+  <div w-screen h-screen flex justify-center items-center>
+    <a-result status="404" class="mb-1/10">
+      <template #title>
+        <div text-32px>
+          您要找的页面不存在
+        </div>
+      </template>
+      <template #extra>
+        <a-space>
+          <a-button type="primary" @click="back">
+            返回
+          </a-button>
+        </a-space>
+      </template>
+    </a-result>
   </div>
 </template>
--- src/pages/exception/500/index.vue
+++ src/pages/exception/500/index.vue
@@ -1,5 +1,31 @@
+<script setup lang="ts">
+const router = useRouter()
+function back() {
+  router.push('/Foo')
+}
+function reTry() {
+  router.back()
+}
+</script>
+
 <template>
-  <h1 text-40px>
-    Error
-  </h1>
+  <div w-screen h-screen flex justify-center items-center>
+    <a-result status="500" subtitle="您可以选择重试或者返回" class="mb-1/10">
+      <template #title>
+        <div text-32px>
+          服务器开小差了...
+        </div>
+      </template>
+      <template #extra>
+        <a-space>
+          <a-button type="secondary" @click="reTry">
+            重试
+          </a-button>
+          <a-button type="primary" @click="back">
+            返回
+          </a-button>
+        </a-space>
+      </template>
+    </a-result>
+  </div>
 </template>
