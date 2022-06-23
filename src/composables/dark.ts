872858b7148b859916bc407446c4a73c9a9b1adc
faitsse <2138889191@qq.com>
2022-06-23 10:57:37 +0800

    chore: update

--- src/composables/dark.ts
+++ src/composables/dark.ts
@@ -2,6 +2,10 @@ export const isDark = useDark()
 export const toggleDark = useToggle(isDark)
 export const preferredDark = usePreferredDark()
 
+/**
+ * when the dark changed, we should
+ * change the app theme immediately.
+ */
 watchEffect(() => {
   if (isDark.value) {
     // set dark theme
