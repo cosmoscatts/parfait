632bd6420981a52de9c2f564af94433945eb7af2
faitsse <2138889191@qq.com>
2022-07-10 17:24:29 +0800

    chore: update

--- src/layouts/widgets/BackTop.vue
+++ src/layouts/widgets/BackTop.vue
@@ -18,7 +18,7 @@ watch(fixHeader, () => {
   <a-back-top :key="key" :target-container="targetContainer">
     <a-button
       fixed shadow-md
-      class="bottom-23% lt-lg:!bottom-18% right-3%"
+      class="bottom-23% lt-lg:!bottom-18% xs:!bottom-10% right-3%"
       size="large" shape="circle"
     >
       <icon-to-top />
