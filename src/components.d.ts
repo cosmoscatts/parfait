2d6279b502e0a43333049e6c6645cd5b17801fb6
faitsse <2138889191@qq.com>
2022-07-15 10:12:24 +0800

    chore: update

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,7 +5,6 @@ import '@vue/runtime-core'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    ASpin: typeof import('@arco-design/web-vue')['Spin']
     AvatarUpload: typeof import('./components/AvatarUpload.vue')['default']
     DarkToggle: typeof import('./components/DarkToggle.vue')['default']
     Plum: typeof import('./components/Plum.vue')['default']
