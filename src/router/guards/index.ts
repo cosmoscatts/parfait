f6c8a58350d6968e8a5749a3b0bb82c2588170b2
faitsse <2138889191@qq.com>
2022-07-22 09:41:11 +0800

    chore: update

--- src/router/guards/index.ts
+++ src/router/guards/index.ts
@@ -1,17 +1,8 @@
 import type { Router } from 'vue-router'
 import setupLoginGuard from './setupLoginGuard'
 import setupPermissionGuard from './setupPermissionGuard'
-import { setRouteEmitter } from '~/utils'
-
-function setupPageGuard(router: Router) {
-  router.beforeEach(async (to) => {
-    // emit route change
-    setRouteEmitter(to)
-  })
-}
 
 export default function createRouterGuard(router: Router) {
-  setupPageGuard(router)
   setupLoginGuard(router)
   setupPermissionGuard(router)
 }
