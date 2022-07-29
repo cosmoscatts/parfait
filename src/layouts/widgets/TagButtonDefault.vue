2d75acd978a2c979f80499e2c2fcac8d7e02ad91
faitsse <2138889191@qq.com>
2022-07-29 14:35:19 +0800

    chore: update

---
+++ src/layouts/widgets/TagButtonDefault.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const {
+  title = '',
+  isActive = false,
+} = defineProps<{
+  title?: string
+  isActive?: boolean
+}>()
+</script>
+
+<template>
+  <span flex="~ nowrap" justify-center items-center lt-sm="!min-w-70px">
+    <span v-if="isActive" i-ri-price-tag-3-fill mr-1 />
+    <span v-else i-ri-price-tag-3-line mr-1 />
+    {{ title }}
+    <slot name="close" />
+  </span>
+</template>
