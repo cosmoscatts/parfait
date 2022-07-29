1b7fe66f0adc02068731b12a0dbf7883c759f1d2
faitsse <2138889191@qq.com>
2022-07-29 16:10:24 +0800

    chore: update

--- src/utils/colorTools.ts
+++ src/utils/colorTools.ts
@@ -1,4 +1,11 @@
-import { colord } from 'colord'
+import { colord, extend } from 'colord'
+import mixPlugin from 'colord/plugins/mix'
+
+extend([mixPlugin])
 export function addColorAlpha(color: string, alpha: number) {
   return colord(color).alpha(alpha).toHex()
+}
+
+export function useMixinColor(firstColor: string, secondColor: string, ratio: number) {
+  return colord(firstColor).mix(secondColor, ratio).toHex()
 }
