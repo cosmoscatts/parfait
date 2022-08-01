9037159d1819bad45189cf7fe8fec37994510d87
faitsse <2138889191@qq.com>
2022-08-01 15:35:13 +0800

    chore: update

---
+++ src/composables/scrollTop.ts
@@ -0,0 +1,21 @@
+import BTween from 'b-tween'
+
+// https://github.com/PengJiyuan/b-tween
+export function scrollToTop(target: HTMLElement, easing = 'quartOut', duration = 500) {
+  if (!target)
+    return
+
+  const { scrollTop } = target
+  const tween = new BTween({
+    from: { scrollTop },
+    to: { scrollTop: 0 },
+    easing,
+    duration,
+    onUpdate: (keys: any) => {
+      if (target)
+        target.scrollTop = keys.scrollTop
+    },
+  })
+  tween.start()
+}
+
