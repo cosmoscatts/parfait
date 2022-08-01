15c0d229953250bcc5c6553b5bcd3cfd09525aae
faitsse <2138889191@qq.com>
2022-08-01 10:36:59 +0800

    chore: update style

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import type { AppContext, UserModule } from './types'
 
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
-import './styles/prose.css'
-import './styles/markdown.css'
 import 'uno.css'
 
 function init(app: AppContext) {
