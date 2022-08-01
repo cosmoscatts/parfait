4b7a943a579d8ee8edbd977f02d0c89c8256860c
faitsse <2138889191@qq.com>
2022-08-01 10:31:23 +0800

    chore: update scroll bar

--- src/components/WaitForWindowReload.vue
+++ src/components/WaitForWindowReload.vue
@@ -1,12 +1,57 @@
 <template>
-  <div
-    h-screen w-screen z-10000 fixed top-0 left-0
-    bg="white dark:#17171A" flex="~ col" justify-center items-center
-  >
-    <div i-carbon-logo-pinterest class="bg-[rgb(var(--primary-6))]" text-80px />
-    <a-spin dot :size="24" class="mt-1/10" />
-    <div text-36px mt-120px font-bold>
-      Parfait
-    </div>
+  <div class="wrapper" bg="white dark:[#17171A]">
+    <div class="mask" />
+    <div class="logo" i-carbon-campsite text=" white" />
   </div>
 </template>
+
+<style scoped>
+.wrapper {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  animation-name: scale;
+  animation-duration: 1.5s;
+  animation-timeline: box-move;
+}
+.mask {
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(-75deg, #63E2B7, #1F8F93);
+  z-index: 2;
+  animation-name: reOpacityChange;
+  animation-duration: 1.5s;
+  animation-timeline: box-move;
+}
+.logo {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(calc(-50% - 2px), calc(-50% - 2px));
+  width: 198px;
+  height: 282px;
+  z-index: 3;
+  animation-name: reOpacityChange;
+  animation-duration: 1.5s;
+  animation-timeline: box-move;
+}
+@keyframes reOpacityChange {
+  0%,
+  40% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+@keyframes scale {
+  0%,
+  40% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(3);
+  }
+}
+</style>
