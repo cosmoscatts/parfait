6e7a2c96961be995b5ad3f8e65e19702106bf1bb
faitsse <2138889191@qq.com>
2022-09-08 19:52:31 +0800

    chore: update

--- src/layouts/BaseLayoutHorizontal.vue
+++ src/layouts/BaseLayoutHorizontal.vue
@@ -30,27 +30,5 @@ watch(() => route.path, (val, old) => {
 </script>
 
 <template>
-  <a-layout id="main-wrapper" ref="refMainWrapper" h-screen w-screen bg-base of="x-hidden y-auto">
-    <a-layout-header bg-header :class="fixHeader ? 'fixed top-0 right-0 w-full' : ''">
-      <TheNav w-full h-50px bg-transparent />
-      <TheTags v-show="showTheTags" w-full h-40px bg-transparent />
-    </a-layout-header>
-    <a-layout
-      id="content-wrapper"
-      ref="refContentWrapper"
-      bg-transparent
-      :class="
-        fixHeader
-          ? showTheTags
-            ? 'of-x-hidden of-y-auto !mt-90px'
-            : 'of-x-hidden of-y-auto !mt-50px'
-          : '' "
-    >
-      <a-layout-content>
-        <TheMain w-full h-full p-5 />
-      </a-layout-content>
-    </a-layout>
-    <TheRightPanel v-model:visible="rightPanelVisible" />
-  </a-layout>
-  <BackTop :target-container="backTopTarget" />
+  1
 </template>
--- src/layouts/base-layout/Horizontal.vue
+++ src/layouts/base-layout/Horizontal.vue
@@ -6,6 +6,29 @@ import {
   TheSettings,
   TheTabs,
 } from '../components'
+
+const { showTheTags, fixHeader } = storeToRefs(useAppStore())
+const rightPanelVisible = ref(false)
+const backTopTarget = computed(() => {
+  return fixHeader.value
+    ? '#content-wrapper'
+    : '#main-wrapper'
+})
+
+// handle scroll position
+const refMainWrapper = ref()
+const refContentWrapper = ref()
+const route = useRoute()
+watch(() => route.path, (val, old) => {
+  if (val === old)
+    return
+  const refTarget = fixHeader.value
+    ? refContentWrapper
+    : refMainWrapper
+  if ((refTarget.value?.$el?.scrollTop ?? 0) === 0)
+    return
+  useScrollTop(refTarget.value!.$el)
+})
 </script>
 
 <template>
