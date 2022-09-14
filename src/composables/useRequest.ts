75caee947612fdd62d5742705c3fec04c0193386
faitsse <2138889191@qq.com>
2022-09-14 10:49:54 +0800

    chore: update

--- src/composables/useRequest.ts
+++ src/composables/useRequest.ts
@@ -42,7 +42,7 @@ function handleRequestError(fn: Fn) {
     data => data,
     (error) => {
       console.error(error)
-      Message.error(error)
+      Message.error('服务器异常')
       router.push(ERROR_PAGE)
       return undefined
     },
